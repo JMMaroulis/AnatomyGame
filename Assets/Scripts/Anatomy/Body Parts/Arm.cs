973c08366d0cfb13999098fe18b61295c00e9ea0
unknown <jamesmaroulis@gmail.com>
2020-01-07 22:14:43 +0000

    Basic scripting for body parts and blood circulation started

---
+++ Assets/Scripts/Anatomy/Body Parts/Arm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour, BodyPart
+{
+    int BodyPart.blood { get => blood; set => blood = value; }
+    private int blood = 500;
+
+    int BodyPart.bloodLossRate { get => bloodLossRate; set => bloodLossRate = value; }
+    private int bloodLossRate = 0;
+
+    //TODO: populate List<BodyPart> from a List<GameObject> as defined in the scene on start()
+    List<BodyPart> BodyPart.connectedBodyParts { get => connectedBodyParts; set => connectedBodyParts = value; }
+    private List<BodyPart> connectedBodyParts = new List<BodyPart>();
+    public List<GameObject> connectedBodyPartsGameObjects;
+
+    //TODO: add a check for running out of blood
+    void BodyPart.pumpBlood()
+    {
+        foreach (BodyPart connectedBodyPart in connectedBodyParts)
+        {
+            blood -= 1;
+            connectedBodyPart.blood += 1;
+        }
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Anatomy/Body Parts/BodyPart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface BodyPart
+{
+    int blood { get; set; }
+    int bloodLossRate { get; set; }
+
+    List<BodyPart> connectedBodyParts {get; set ;}
+
+    void pumpBlood();
+}
