b92f82a70769cdf66ebe427591b6a1dd29bbd640
unknown <jamesmaroulis@gmail.com>
2020-01-22 19:09:01 +0000

    Added damage level reporting

---
+++ Assets/Scripts/UI/DamageLevelReporting.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageLevelReporting : MonoBehaviour
+{
+    public GameObject body;
+    private List<GameObject> bodyPartObjects = new List<GameObject>();
+    private float secondCounter;
+    private Text damageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get bodyparts from body
+        for (int i = 0; i < body.transform.childCount; i++)
+        {
+            bodyPartObjects.Add(body.transform.GetChild(i).gameObject);
+
+            //get organs from bodypart
+            //NOTE: THIS INTRODUCES THE ASSUMPTION THAT ORGANS WILL ALWAYS BE CHILDREN OF THEIR CONTAINING BODYPARTS
+            //AS WELL AS CONTIANED IN THE containedOrgans LISTS. IT WILL DO FOR NOW.
+            for(int j = 0; j < body.transform.GetChild(i).childCount; j++)
+            {
+                bodyPartObjects.Add(body.transform.GetChild(i).GetChild(j).gameObject);
+            }
+        }
+
+        damageText = gameObject.GetComponent<Text>();
+        UpdateDamageText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        secondCounter += Time.deltaTime;
+        if (secondCounter >= 1.0f)
+        {
+            UpdateDamageText();
+            secondCounter -= 1.0f;
+
+        }
+
+
+    }
+
+    void UpdateDamageText()
+    {
+        string damageTextNew = "Damage Levels:\n";
+        float damageSum = 0;
+        foreach (GameObject bodyPartObject in bodyPartObjects)
+        {
+            damageTextNew += bodyPartObject.name;
+            damageTextNew += ": ";
+            damageTextNew += Mathf.Round(bodyPartObject.GetComponent<BodyPart>().damage);
+            damageTextNew += "\n";
+            damageSum += bodyPartObject.GetComponent<BodyPart>().damage;
+        }
+        damageTextNew += "Sum Damage: " + damageSum;
+
+        damageText.text = damageTextNew;
+
+    }
+}
