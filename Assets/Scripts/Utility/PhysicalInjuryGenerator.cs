1adc5cf39c6ea798c886641986bdb79305ce3e03
unknown <jamesmaroulis@gmail.com>
2020-03-29 23:18:18 +0100

    Rudimentary random injury generation

---
+++ Assets/Scripts/Utility/PhysicalInjuryGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalInjuryGenerator : MonoBehaviour
+{
+    public GameObject body;
+    private List<BodyPart> bodyParts;
+    private List<Organ> organs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateBodyPartsList();
+        InjureRandomBodypart(2);
+    }
+
+    //select n random bodyparts
+    public void InjureRandomBodypart(int numberOfInjuries)
+    {
+        for (int i = 0; i < numberOfInjuries; i++)
+        {
+            //selected bodypart to injure, and injury to apply
+            BodyPart bodyPart = bodyParts[Random.Range(0, bodyParts.Count)];
+            int injuryNumber = Random.Range(0, 3);
+
+            //apply injury
+            switch (injuryNumber)
+            {
+                case 0:
+                    Debug.Log($"Stabbed {bodyPart.name}");
+                    Stab(bodyPart);
+                    break;
+
+                case 1:
+                    Debug.Log($"Shot {bodyPart.name}");
+                    Shoot(bodyPart);
+                    break;
+
+                case 2:
+                    Debug.Log($"Crushed {bodyPart.name}");
+                    Crush(bodyPart);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    //assorted physical injury methods
+    public void Stab(BodyPart bodyPart)
+    {
+        bodyPart.damage = Mathf.Min(bodyPart.damage + 30, bodyPart.damageMax);
+        bodyPart.bloodLossRate += 10;
+    }
+
+    public void Shoot(BodyPart bodyPart)
+    {
+        bodyPart.damage = Mathf.Min(bodyPart.damage + 30, bodyPart.damageMax);
+        bodyPart.bloodLossRate += 10;
+    }
+
+    public void Crush(BodyPart bodyPart)
+    {
+        bodyPart.damage = Mathf.Min(bodyPart.damage + 30, bodyPart.damageMax);
+        bodyPart.bloodLossRate += 10;
+    }
+
+
+
+    void PopulateBodyPartsList()
+    {
+
+        bodyParts = new List<BodyPart>();
+
+        //get bodyparts from body
+        for (int i = 0; i < body.transform.childCount; i++)
+        {
+            bodyParts.Add(body.transform.GetChild(i).GetComponent<BodyPart>());
+        }
+
+    }
+}
