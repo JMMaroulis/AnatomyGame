e906531e6c126702df1cf1e06faa434b9e9270d0
unknown <jamesmaroulis@gmail.com>
2020-04-23 15:43:24 +0100

    Added charm.cs and heartcharm.cs

---
+++ Assets/Scripts/Charms/Charm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Charm : MonoBehaviour
+{
+
+    public float expiryTime;
+    public float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0.0f;
+        expiryTime = 60.0f * 30.0f;
+    }
+
+    public void CharmTimer(float seconds)
+    {
+        timeElapsed += seconds;
+        if (timeElapsed >= expiryTime)
+        {
+            Destroy(this);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Charms/HeartCharm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HeartCharm : Charm
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.GetComponent<BodyPart>().isTimePassing)
+        {
+            float deltaTime = Time.deltaTime;
+            //capping deltatime at 100ms to stop inaccuracies
+            while (deltaTime > 0.0f)
+            {
+                float tempDeltaTime = Mathf.Min(deltaTime, 0.2f);
+
+                PumpBloodMaster(tempDeltaTime);
+                CharmTimer(tempDeltaTime);
+
+                deltaTime = Mathf.Max(0.0f, deltaTime - 0.2f);
+            }
+
+        }
+    }
+
+    //stolen from Heart.cs, modified slightly to work as an attachment to a bodypart rather than a bodypart in and of itself
+    //efficiency forcibly set to 1.0f
+    void PumpBloodMaster(float deltaTime)
+    {
+        //starting from a random bodypart that is connected to this charm:
+        BodyPart[] allBodyParts = FindObjectsOfType<BodyPart>();
+        List<int> bodypartOrder = Enumerable.Range(0, allBodyParts.Count()).ToList<int>();
+        IListExtensions.Shuffle<int>(bodypartOrder);
+
+        foreach (int bodypartIndex in bodypartOrder)
+        {
+            if (allBodyParts[bodypartIndex].IsConnectedToBodyPartStarter(this.gameObject.GetComponent<BodyPart>()))
+            {
+                allBodyParts[bodypartIndex].PumpBlood(1.0f, deltaTime);
+            }
+        }
+    }
+}
