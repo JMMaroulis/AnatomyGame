a5c647d946e02c2f72a4602660c5c3eccf71f7a0
unknown <jamesmaroulis@gmail.com>
2020-04-27 20:05:40 +0100

    Added preload scene

---
+++ Assets/ddol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ddol : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Yell()
+    {
+        Debug.Log("Yelling into the void!");
+    }
+}
