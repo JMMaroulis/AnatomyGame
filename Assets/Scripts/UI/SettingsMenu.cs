edbd11ad7ca074d84fdcc933c4af1cf8c311be12
unknown <jamesmaroulis@gmail.com>
2020-05-12 18:44:04 +0100

    Very (no, really, *very*) basic quit button + menu

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void CloseDialogue()
+    {
+        Debug.Log("go back");
+
+        this.gameObject.SetActive(false);
+    }
+
+    public void OpenDialogue()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
